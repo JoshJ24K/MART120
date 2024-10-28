2dd7621e4f584c7e515061f31f04ae1fa2dd3185
JoshJ24K <jj207088@umconnect.umt.edu>
2024-10-28 17:18:25 -0600

    Added Homework Week 9
    Added folder I made for VS Code, it should work right. I copied exactly what I made there into my Github repository.

---
+++ Homework 9/Week9Sketch.js
@@ -0,0 +1,56 @@
+function setup()
+            {
+                createCanvas(700,700);
+            }
+            function draw()
+            {
+                background(200);
+                strokeWeight(3.5)
+                fill(255, 223, 217);
+                circle(350,350,500);
+                fill(0);
+                rect(200,275,110,75);
+                fill(255,223,217);
+                rect(210,281,90,60);
+                fill(0);
+                rect(400,275,110,75);
+                fill(255,223,217);
+                rect(410,281,90,60);
+                fill(0);
+                rect(300,305,100,10);
+                fill(255, 255, 255);
+                ellipse(255,310,65,45)
+                fill(84, 192, 255);
+                circle(255,310,40);
+                fill(0);
+                circle(255,310,15);
+                fill(255, 255, 255);
+                ellipse(455,310,65,45)
+                fill(84, 192, 255);
+                circle(455,310,40);
+                fill(0);
+                circle(455,310,15);
+                fill(71, 38, 6)
+                circle(150,150,200)
+                fill(71, 38, 6)
+                circle(335,150,200)
+                fill(71, 38, 6)
+                circle(515,150,200)
+                fill(71, 38, 6)
+                circle(420,100,150)
+                fill(71, 38, 6)
+                circle(240,100,150)
+                fill(255, 223, 217)
+                triangle(450, 400, 325, 350, 345, 400);
+                line(325, 500, 400, 500);
+                line(400,500,450,471)
+                line(325, 500, 280, 471);
+                point(350,397)
+                point(400,397)
+                fill(0)
+                textSize(35)
+                text('-Joshua Johnson', 400, 650);
+                textSize(20)
+                text('This does not look like me but I tried!', 15,650 )
+
+            }
