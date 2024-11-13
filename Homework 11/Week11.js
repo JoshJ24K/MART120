4f312b0f7a300b94d04af8b487c1dd83d53ce964
JoshJ24K <jj207088@umconnect.umt.edu>
2024-11-12 18:27:26 -0700

    Added Homework 11
    Added "Homework 11" folder with HTML and JS files
    Added Kahoot results screenshot

---
+++ Homework 11/Week11.js
@@ -0,0 +1,191 @@
+
+// x and y for my character
+var characterX = 100;
+var characterY = 100;
+// define the key codes for each letter
+var w = 87; 
+var s = 83;
+var a = 65;
+var d = 68;
+
+// x and y for a shape
+var shapeX = 30;
+var shapeY = 50;
+var shapeX2 = 60;
+var shapeY2 = 75;
+var shapeXSpeed;
+var shapeYSpeed;
+var shapeXSpeed2;
+var shapeYSpeed2;
+
+// create a shape when the mouse is clicked
+var mouseShapeX;
+var mouseShapeY;
+function setup()
+{
+    createCanvas(500, 600);
+    // get a random speed when the it first starts
+    shapeXSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+    shapeYSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+    createCharacter(200,350);
+}
+
+function draw()
+{
+    background(154, 162, 173);
+    stroke(0);
+    fill(0);
+    
+    // call createBorders function
+    createBorders(10);
+
+    // exit message
+    textSize(16);
+    text("EXIT", width-50,height-50)
+
+    //createCharacter(200,350);
+    drawCharacter();
+    characterMovement();
+
+
+    // potential enemy
+    fill(255, 0, 0);
+    // draw the shape
+    circle(shapeX, shapeY, 50);
+
+     // get a random speed when the it first starts
+     shapeXSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+     shapeYSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+
+    // enemy 2
+    fill(255, 0, 166)
+    square(shapeX2, shapeY2, 80)
+    // enemy 2 random speed
+    shapeXSpeed2 = Math.floor(Math.random() * (Math.floor(Math.random() * 8)) + 1);
+     shapeYSpeed2 = Math.floor(Math.random() * (Math.floor(Math.random() * 9)) + 1);
+
+
+    // move the shape
+    shapeX += shapeXSpeed;
+    shapeY += shapeYSpeed;
+    //move enemy 2
+    shapeX2 += shapeXSpeed2;
+    shapeY2 += shapeYSpeed2;
+    // check to see if the shape has gone out of bounds
+    if(shapeX > width)
+    {
+        shapeX = 0;
+    }
+    if(shapeX < 0)
+    {
+        shapeX = width;
+    }
+    if(shapeY > height)
+    {
+        shapeY = 0;
+    }
+    if(shapeY < 0)
+    {
+        shapeY = height;
+    }
+    if(shapeX2 > width)
+        {
+            shapeX2 = 0;
+        }
+        if(shapeX2 < 0)
+        {
+            shapeX2 = width;
+        }
+        if(shapeY2 > height)
+        {
+            shapeY2 = 0;
+        }
+        if(shapeY2 < 0)
+        {
+            shapeY2 = height;
+        }
+
+    // check to see if the character has left the exit
+    if(characterX > width && characterY > width-50)
+    {
+        fill(0);
+        stroke(5);
+        textSize(26);
+        text("You did it!", width/2-50, height/2-50);
+    }
+
+    // create the shape based on the mouse click
+    fill(255, 251, 0);
+    circle(mouseShapeX, mouseShapeY, 80);
+}
+
+function characterMovement()
+{
+    // handle the keys
+    if(keyIsDown(w))
+    {
+        characterY -= 5;   
+    }
+    if(keyIsDown(s))
+    {
+        characterY += 5;   
+    }
+    if(keyIsDown(a))
+    {
+        characterX -= 5;   
+        console.log("movement: " + characterX);
+    }
+    if(keyIsDown(d))
+    {
+        characterX += 5;   
+    }
+}
+function createCharacter(x,y)
+{
+    characterX = x;
+    characterY = y;
+    console.log(characterX);
+    //character
+    
+   // circle(characterX,characterY,25);
+}
+
+function drawCharacter()
+{
+    fill(0);
+    square(characterX,characterY,45);
+}
+function createBorders(thickness)
+{
+    // top border
+    rect(0,0,width,thickness);
+    // left border
+    rect(0,0,thickness,height);
+    // bottom border
+    rect(0, height-thickness,width, thickness);
+    // right upper border
+    rect(width-thickness,0,thickness,height-50);
+}
+
+function mouseClicked()
+{
+    mouseShapeX = mouseX;
+    mouseShapeY = mouseY;
+}
+/*
+function keyPressed() {
+    if (keyCode === LEFT_ARROW) {
+        characterX -= 10;
+    } 
+    else if (keyCode === RIGHT_ARROW) {
+        characterX += 10;
+    }
+    else if (keyCode === UP_ARROW) {
+        characterY -= 10;
+    }
+    else if (keyCode === DOWN_ARROW) {
+        characterY += 10;
+    }
+
+  }
+  */
