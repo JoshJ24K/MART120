cfe48798a1abc7df657498c2fd2c36b532e62af4
JoshJ24K <jj207088@umconnect.umt.edu>
2024-12-13 23:45:14 -0700

    Added Final Folder
    Added Final folder

---
+++ Homework 9/Final.js
@@ -0,0 +1,24 @@
+<style>
+    span
+    {
+    background-color:
+    rgb(255, 247, 0);
+    }
+  </style>
+<body>
+    <h1>Final Portfolio!</h1>
+    <p style="font-family:'Times New Roman', Times, serif">This was a great class! I am proud of what I have created this semester! This is where I will show everything I've done! </p>
+    <br /> 
+    
+</body>
+</head> 
+<span class="blueColor">Here are the links for my other projects!</span class="blueColor"><body>
+<ol> 
+    <li><a href="https://studio.code.org/projects/applab/L4YaC4MzRvNL8n5lRl1jh47OiC40W3A9TpNK-auX-yc"> Cat App </a>
+    <li><a href= "https://studio.code.org/projects/playlab/6tr83-T_5iJTRBlHSJyH2Gbv3qovNgsbdm0JzH1Dou4"> Catch the Wizard!</a></li>
+    <li><a href= "https://youtu.be/keQYMBG0Eb0"> Alice Project</a></li>
+    <li><a href="https://github.com/JoshJ24K/MART120/tree/main/Homework%209"> Self Portrait</a></li>
+    <li><a href= "https://github.com/JoshJ24K/MART120/tree/main/Homework%2010"> Moving Self Portrait</a></li>
+    <li><a href="https://github.com/JoshJ24K/MART120/tree/main/Homework%2011"> Moving Game</a></li>
+
+</ol>
